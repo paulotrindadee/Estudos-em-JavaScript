a78f81b5877c941e75a830e58701bb85403d8e90
paulotrindadee <paulotrindade90@outlook.com>
2023-12-08 00:06:35 -0300

    aula sobre Arrays e estrutura de repetição

---
+++ Arrays e Estruturas de Repetição em JavaScript/Aula.js
@@ -0,0 +1,14 @@
+// // Forma de objeto literal
+// // const alunos {nome, idade, materia}
+
+// const alunos = ['1', '2', '3']
+// console.log(alunos)
+
+// //Adicionar um item a minha lista
+// // alunos.push('Renan')
+// // alunos[3] = 'Vinicius'
+
+// console.log(alunos.pop())
+
+// // Se quiser pegar apenas um 'Aluno' chamo dentro da []
+// console.log(alunos)
