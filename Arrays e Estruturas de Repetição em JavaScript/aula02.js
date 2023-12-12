61bcca3c0c6b6e2d56a6a33156a7e7aee4b5cf98
paulotrindadee <paulotrindade90@outlook.com>
2023-12-12 20:51:43 -0300

    Concluindo mais um módulo nos estudos de JavaScript pela DIO  (Desenvolvimento FrontEnd com JavaScript e Angulas)

--- Arrays e Estruturas de Repetição em JavaScript/aula02.js
+++ Arrays e Estruturas de Repetição em JavaScript/aula02.js
@@ -8,4 +8,13 @@ notas.push(5)
 
 const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4]
 
-console.log(soma / 5)
+// Funciona para percorrer uma quantidade especifica de vezes
+// Exemplo  do 0 a 10 vezes
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+// let i = 0  Declaração da variavel
+// 0; i Estrutura condicional
+// i++ É incremento para a estrutura condicional. Oque  vai ser executado depois que o FOR acabar.
---
+++ Arrays e Estruturas de Repetição em JavaScript/aula04.js
@@ -0,0 +1,22 @@
+// Depurar o nosso código
+// Verificando linha por linha
+
+const notas = []
+
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+notas.push(8)
+
+let soma = 0
+//Estrutura de repetição
+
+for (let i = 0; i < notas.length; i++) {
+  const nota = notas[i]
+  soma = soma + nota
+}
+
+const media = soma / notas.length
+console.log(media)
